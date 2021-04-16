c3f1323bb2c78aa639ec95e518aab2683fc47206
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-04-16 10:16:14 +0700

    Exclude ItemSeeder from DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(UnitSeeder::class);
 
-        $this->call(ItemSeeder::class);
+        // $this->call(ItemSeeder::class);
     }
 }
